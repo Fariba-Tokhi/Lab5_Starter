6cf346d5a9bf49f0b5146baf8b1a1c3c0dd0eb53
Friba Tokhi <fribatokhi@Fribas-MacBook-Pro.local>
2026-05-05 21:21:20 -0700

    expose.js added

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,7 +1,54 @@
-// expose.js
-
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  // Select elements
+  const hornSelect = document.getElementById('horn-select');
+  const hornImage = document.querySelector('#expose img');
+  const audio = document.querySelector('audio');
+
+  const volumeSlider = document.getElementById('volume');
+  const volumeIcon = document.querySelector('#volume-controls img');
+
+  const playButton = document.querySelector('button');
+
+  // Change horn
+  hornSelect.addEventListener('change', () => {
+    const selectedHorn = hornSelect.value;
+
+    hornImage.src = `assets/images/${selectedHorn}.svg`;
+    audio.src = `assets/audio/${selectedHorn}.mp3`;
+  });
+
+  // Change volume
+  volumeSlider.addEventListener('input', () => {
+    const volume = Number(volumeSlider.value);
+
+    // Set actual audio volume (0–1)
+    audio.volume = volume / 100;
+
+    // Change volume icon
+    if (volume === 0) {
+      volumeIcon.src = 'assets/icons/volume-level-0.svg';
+    } 
+    else if (volume < 33) {
+      volumeIcon.src = 'assets/icons/volume-level-1.svg';
+    } 
+    else if (volume < 67) {
+      volumeIcon.src = 'assets/icons/volume-level-2.svg';
+    } 
+    else {
+      volumeIcon.src = 'assets/icons/volume-level-3.svg';
+    }
+  });
+
+  // Play sound
+  playButton.addEventListener('click', () => {
+    audio.play();
+
+    // Party horn = confetti
+    if (hornSelect.value === 'party-horn') {
+      const jsConfetti = new JSConfetti();
+      jsConfetti.addConfetti();
+    }
+  });
 }
